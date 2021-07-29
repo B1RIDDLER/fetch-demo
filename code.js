0c6f720094883c7e72c1569e3b1344d4ad367fc0
B1RIDDLER <b1riddler@gmail.com>
2021-07-29 14:40:59 -0500

    set up our eventListener on the button element; set up a static fetch request

---
+++ code.js
@@ -0,0 +1,16 @@
+// Selecting our HTML elements
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const countryDiv = document.querySelector('.country-div');
+
+// Add an event listener to our button
+button.addEventListener('click', function () {
+// Fetch our data from
+fetch("https://restcountries.eu/rest/v2/name/usa")
+    .then((response) => response.json())
+    .then((data) => {
+        const countryData = data[0];
+        // destructuring version: const [countryData] = data;
+    });
+
+});
